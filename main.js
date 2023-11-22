a4391d3c402fff781416ca448cf401ecd745f1f2
AryamanKalia <aryamankalia1729@gmail.com>
2023-11-23 01:47:38 +0530

    Add navigation and content visibility toggle Added a navigation bar to the index.html file to improve user navigation. The navigation bar includes links to the Skills, Projects, and Work Experience sections. Implemented a content visibility toggle in main.js. This function hides all content sections by default and only displays the section corresponding to the clicked navigation link. Updated the CSS in style.css to support the new navigation bar and content visibility toggle. This includes styles for the navigation bar and the hover effect for navigation links. also, reorganized the structure of the index.html file for better readability and maintenance.

--- main.js
+++ main.js
@@ -25,3 +25,11 @@ function typeWriter() {
 }
 
 typeWriter();
+
+function showContent(id) {
+  var contents = document.getElementsByClassName('content');
+  for (var i = 0; i < contents.length; i++) {
+    contents[i].style.display = 'none';
+  }
+  document.getElementById(id).style.display = 'block';
+}
